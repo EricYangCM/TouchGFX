1ada37ee135cbfcb4495b194a1b187950ccd8dc2
EricYangCM <106502039+EricYangCM@users.noreply.github.com>
2025-03-18 13:21:48 +0900

    Update TouchGFX_Model_Events.c

--- TouchGFX_Model_Events.c
+++ TouchGFX_Model_Events.c
@@ -1,5 +1,5 @@
 /*
- * TouchGFX_Model_Events.c
+ * UI_Ctrl.c
  * Created on: Mar 18, 2025
  * Author: cyang
  */
@@ -10,11 +10,18 @@
 
 // ESC 버튼 이벤트 상태 저장 변수
 ESC_Btn_Pressed_t ESC_Btn_Pressed = {0, 0};
-
-// ESC 버튼 이벤트 발생 설정
-void TouchGFX_ModelEvents_Set_ESC_Btn_Pressed(int state)
+void TouchGFX_Set_Event_ESC_Btn_Pressed(int state)
 {
 	ESC_Btn_Pressed.State = state;   // 상태 값 저장
     ESC_Btn_Pressed.IsEventSet = 1;  // 이벤트 발생 플래그 설정
 }
 
+
+
+// 스크린 이동
+Go_to_Screen_t Go_to_Screen = {0, 0};
+void TouchGFX_Set_Event_Go_to_Screen(int state);
+void TouchGFX_Go_to_Screen(int ScreenNum)
+{
+
+}
