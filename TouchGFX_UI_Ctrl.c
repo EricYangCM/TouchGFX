07f5b3dde2ceb05f546e0d516208fa3ef726e898
EricYangCM <106502039+EricYangCM@users.noreply.github.com>
2025-03-18 11:04:29 +0900

    Create TouchGFX_UI_Ctrl.c

---
+++ TouchGFX_UI_Ctrl.c
@@ -0,0 +1,17 @@
+/*
+ * UI_Ctrl.c
+ * Created on: Mar 18, 2025
+ * Author: cyang
+ */
+
+#include "UI_Ctrl.h"
+
+// ESC 버튼 이벤트 상태 저장 변수
+ESC_Btn_Pressed_t ESC_Btn_Pressed = {0, 0};
+
+// ESC 버튼 이벤트 발생 설정
+void TouchGFXUICtrl_Set_Event_ESC_Btn_Pressed(int state)
+{
+    ESC_Btn_Pressed.IsEventSet = 1;  // 이벤트 발생 플래그 설정
+    ESC_Btn_Pressed.State = state;   // 상태 값 저장
+}
