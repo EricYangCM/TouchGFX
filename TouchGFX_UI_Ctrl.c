c41f143f6b70ae6a63f2ece76a0220c570058d1e
EricYangCM <106502039+EricYangCM@users.noreply.github.com>
2025-03-18 11:13:28 +0900

    Update TouchGFX_UI_Ctrl.c

--- TouchGFX_UI_Ctrl.c
+++ TouchGFX_UI_Ctrl.c
@@ -13,6 +13,6 @@ ESC_Btn_Pressed_t ESC_Btn_Pressed = {0, 0};
 // ESC 버튼 이벤트 발생 설정
 void TouchGFXUICtrl_Set_Event_ESC_Btn_Pressed(int state)
 {
-    ESC_Btn_Pressed.IsEventSet = 1;  // 이벤트 발생 플래그 설정
     ESC_Btn_Pressed.State = state;   // 상태 값 저장
+    ESC_Btn_Pressed.IsEventSet = 1;  // 이벤트 발생 플래그 설정
 }
