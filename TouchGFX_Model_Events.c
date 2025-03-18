cf689559757df92d87650d1e1529eee9c19f9279
EricYangCM <106502039+EricYangCM@users.noreply.github.com>
2025-03-18 12:14:14 +0900

    Update and rename TouchGFX_UI_Ctrl.c to TouchGFX_Model_Events.c

--- TouchGFX_Model_Events.c
+++ TouchGFX_Model_Events.c
@@ -7,12 +7,14 @@
 #include "main.h"
 
 
+
 // ESC 버튼 이벤트 상태 저장 변수
 ESC_Btn_Pressed_t ESC_Btn_Pressed = {0, 0};
 
 // ESC 버튼 이벤트 발생 설정
-void TouchGFXUICtrl_Set_Event_ESC_Btn_Pressed(int state)
+void ModelEvents_Set_Event_ESC_Btn_Pressed(int state)
 {
-    ESC_Btn_Pressed.State = state;   // 상태 값 저장
+	ESC_Btn_Pressed.State = state;   // 상태 값 저장
     ESC_Btn_Pressed.IsEventSet = 1;  // 이벤트 발생 플래그 설정
 }
+
